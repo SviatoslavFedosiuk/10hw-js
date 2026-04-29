0e08d0ca28c90c4e78852954f1b4516c78ebe092
SviatoslavFedosiuk <185240326+SviatoslavFedosiuk@users.noreply.github.com>
2026-04-29 15:27:10 +0300

    6th

--- src/js/fourth.js
+++ src/js/fourth.js
@@ -2,10 +2,9 @@
 const input = document.querySelector(".ipt");
 const btn = document.querySelector(".button");
 btn.addEventListener("click", ()=>{
-    let value = input.value * 1000
-    console.log(value);
+    let values = input.value * 1000
     setTimeout(()=>{
-alert(`Your value: ${value}`)
-    }, value)
+alert(`Your value: ${values}`)
+    }, values)
     
 })
